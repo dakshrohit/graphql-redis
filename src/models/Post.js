c9f3be0c694bf3628d2f3f13f9c6da6a2f0a7c28
daksh <dakshrohitsunkara@gmail.com>
2025-12-18 04:32:01 +0530

    add user and post models using mongoose schema

---
+++ src/models/Post.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Post || mongoose.model("Post", PostSchema);
---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const UserSchema=new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+        trim:true
+    },
+
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+},
+{
+    timestamps:true
+
+
+}
+);
+export const User=mongoose.models.User || mongoose.model("User",UserSchema);
