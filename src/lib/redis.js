9bcf4d2ef1800c96e230d714820b6d8612da85a4
daksh <dakshrohitsunkara@gmail.com>
2025-12-18 03:58:35 +0530

    add redis connection setup logic

---
+++ src/lib/redis.js
@@ -0,0 +1,10 @@
+import { createClient } from "redis";
+const redis = createClient({
+  url: process.env.REDIS_URL,
+});
+redis.on("error", (err) => {
+  console.log("Redis Client Error", err);
+});
+await redis.connect();
+console.log("Redis connected");
+export default redis;
